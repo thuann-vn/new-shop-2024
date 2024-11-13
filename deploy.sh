92c3fc6b873fef465fa8c1164efceb5313c2931c
Thua Nguyen <thua@vsee.com>
2024-11-13 03:38:35 -0800

    Deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Execute git pull to update the repository
+git pull
+
+# Execute the build script
+export NODE_OPTIONS=--max_old_space_size=4096
+npm run build-no-check
+
+# Clear cache
+php artisan optimize
+php artisan filament:clear-cached-components
+php artisan filament:cache-components
+
+# Restart the queue worker
+php artisan queue:restart
